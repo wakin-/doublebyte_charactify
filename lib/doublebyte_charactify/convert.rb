8a8055b8df10b7fbbc8bdec30a16f417bdf5bf24
wakin- <8th.mail@gmail.com>
2018-04-26 11:57:11 +0900

    add chomp

--- lib/doublebyte_charactify/convert.rb
+++ lib/doublebyte_charactify/convert.rb
@@ -3,7 +3,7 @@ module DoublebyteCharactify
     if !(tags.select { |tag| tag.name === '全角芸' }).blank?
       doc = Nokogiri::XML.parse(content)
       doc.traverse do |e| e.content = e.content.tr('0-9a-zA-Z/\\\.:#\-_"\' ', '０-９ａ-ｚＡ-Ｚ／＼．：＃ー＿”’　') if e.text? end
-      content = doc.to_xhtml(:encoding => 'UTF-8')
+      content = doc.to_xhtml(:encoding => 'UTF-8').chomp
     end
     content
   end
--- lib/doublebyte_charactify/version.rb
+++ lib/doublebyte_charactify/version.rb
@@ -1,3 +1,3 @@
 module DoublebyteCharactify
-  VERSION = "1.1.15"
+  VERSION = "1.1.16"
 end
